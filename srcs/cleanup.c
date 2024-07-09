686919199de6d461ad691bd118e19b294e4a7218
Aziz018 <khatabiaziz707@gmail.com>
2024-07-09 13:39:18 +0100

    fixin a small SIGV

--- srcs/cleanup.c
+++ srcs/cleanup.c
@@ -6,7 +6,7 @@
 /*   By: aelkheta <aelkheta@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/18 15:27:48 by aelkheta          #+#    #+#             */
-/*   Updated: 2024/07/01 16:11:24 by aelkheta         ###   ########.fr       */
+/*   Updated: 2024/07/09 13:20:56 by aelkheta         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,7 +19,7 @@ void	free_array(char **array)
 	i = -1;
 	if (!array)
 		return ;
-	while (array != NULL && array[++i] != NULL)
+	while (array != NULL && array[++i] != NULL && array[i][0] != '\0')
 		free(array[i]);
 	free(array);
 }
